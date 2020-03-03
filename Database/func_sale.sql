5f147dda416354154c9e5947e1ab5c2608014ce1
leonardocfirmino <asdfghjkl.rs70@gmail.com>
2020-03-03 15:24:43 -0300

    Ultimas mudanças

--- Database/func_sale.sql
+++ Database/func_sale.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Mar-2020 às 18:40
+-- Tempo de geração: 03-Mar-2020 às 19:18
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.1
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `func_sale`
 --
+CREATE DATABASE IF NOT EXISTS `func_sale` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `func_sale`;
 
 -- --------------------------------------------------------
 
@@ -36,14 +38,6 @@ CREATE TABLE `funcionario` (
   `cpf_func` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Extraindo dados da tabela `funcionario`
---
-
-INSERT INTO `funcionario` (`id_func`, `nome_func`, `idade_func`, `salario_func`, `cpf_func`) VALUES
-(56, 'Coxinha', 222, '222.22', '222.222.222-22'),
-(57, 'dadede', 222, '2.22', '111.111.111-11');
-
 -- --------------------------------------------------------
 
 --
@@ -57,14 +51,6 @@ CREATE TABLE `venda` (
   `preco_venda` decimal(10,2) NOT NULL,
   `data_venda` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Extraindo dados da tabela `venda`
---
-
-INSERT INTO `venda` (`id_venda`, `cliente_venda`, `vendedor_venda`, `preco_venda`, `data_venda`) VALUES
-(53, 'Kidao gay', 56, '22.22', '2020-03-03 09:52:27'),
-(54, 'sadasdads', 56, '222.22', '2020-03-03 10:37:11');
 
 --
 -- Índices para tabelas despejadas
@@ -82,22 +68,6 @@ ALTER TABLE `funcionario`
 ALTER TABLE `venda`
   ADD PRIMARY KEY (`id_venda`),
   ADD KEY `venda_ibfk_1` (`vendedor_venda`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  MODIFY `id_func` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
-
---
--- AUTO_INCREMENT de tabela `venda`
---
-ALTER TABLE `venda`
-  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- Restrições para despejos de tabelas
